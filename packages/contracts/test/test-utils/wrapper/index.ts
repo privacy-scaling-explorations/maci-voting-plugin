f7eeda18bb1ae50242f984d59bdc5def5d89dc99
NicoSerranoP <nnicosp@hotmail.com>
2025-04-15 12:37:28 -0500

    fix(contracts): delete contracts in Migration.sol

--- packages/contracts/test/test-utils/wrapper/index.ts
+++ packages/contracts/test/test-utils/wrapper/index.ts
@@ -12,22 +12,9 @@ export const ARTIFACT_SOURCES = {
   DAO_V1_0_0: '@aragon/osx-v1.0.1/core/dao/DAO.sol:DAO',
   DAO_V1_3_0: '@aragon/osx-v1.3.0/core/dao/DAO.sol:DAO',
   MajorityVotingMock: 'src/mocks/MajorityVotingMock.sol:MajorityVotingMock',
-  VotingPowerCondition: 'src/VotingPowerCondition.sol:VotingPowerCondition',
-  TokenVoting: 'src/TokenVoting.sol:TokenVoting',
-  TokenVoting_V1_0_0:
-    '@aragon/osx-v1.0.0/plugins/governance/majority-voting/token/TokenVoting.sol:TokenVoting',
-  TokenVoting_V1_3_0:
-    '@aragon/osx-v1.3.0/plugins/governance/majority-voting/token/TokenVoting.sol:TokenVoting',
-  TestGovernanceERC20: 'src/mocks/TestGovernanceERC20.sol:TestGovernanceERC20',
-  GovernanceERC20: 'src/ERC20/governance/GovernanceERC20.sol:GovernanceERC20',
   ERC20Mock: 'src/mocks/ERC20Mock.sol:ERC20Mock',
-  GovernanceWrappedERC20:
-    'src/ERC20/governance/GovernanceWrappedERC20.sol:GovernanceWrappedERC20',
   CustomExecutorMock: 'src/mocks/CustomExecutorMock.sol:CustomExecutorMock',
   ERC20: '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
-  TokenVotingSetup: 'src/TokenVotingSetup.sol:TokenVotingSetup',
-  TokenVotingSetupZkSync:
-    'src/TokenVotingSetupZkSync.sol:TokenVotingSetupZkSync',
   MaciVoting: 'src/MaciVoting.sol:MaciVoting',
   MaciVotingSetup: 'src/MaciVotingSetup.sol:MaciVotingSetup',
 };
